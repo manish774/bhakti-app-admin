340215e5cdfd54fb36a291d3d3cb7660cc6610a5
Manish774 <pnka3196@gmail.com>
2025-12-29 22:17:47 +0530

    Minor . changes

--- src/component/routes/Routes.tsx
+++ src/component/routes/Routes.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
-import Dashboard from "../Dashboard/Dashboard";
 import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 import Login from "../Login/Login";
 import Temple from "../main/temple/Temple";
