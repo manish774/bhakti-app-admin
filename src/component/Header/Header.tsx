e789816de2bf19ce27b55c505581d2e64e1012bd
Manish774 <pnka3196@gmail.com>
2026-05-21 17:57:20 +0530

    dummy commit

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -4,12 +4,12 @@ import "./Header.css";
 interface HeaderProps {
   onMenuClick: () => void;
 }
-
+//Header
 const Header = ({ onMenuClick }: HeaderProps) => {
   const user = useUser();
   const firstLetter = user?.state.meta?.name?.slice(0, 1);
   const logoutButton = () => {
-    localStorage.removeItem("authToken");
+    localStorage.removeItem("authToken.");
     window.location.href = "/login";
   };
   return (
